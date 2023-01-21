42e3571886066ca6f95f7322dc767f116a499643
Samuel Fayad <Samuel.S.Fayad@gmail.com>
2023-01-21 12:04:14 -0600

    updated the create_subset file to define the points of interrogation for the subset as well as the shape function

--- create_subset.m
+++ create_subset.m
@@ -1,7 +1,17 @@
 function subset_struct = create_subset(subset_info)
 
 
+if strcmp(subset_info.shape,'circle')
+else % make it a square
+    subset_struct.points_x = (-(subset_info.size-1)/2:(subset_info.size-1)/2).*ones(subset_info.size);
+    subset_struct.points_y = subset_struct.points_x';
+end
 
+if strcmp(subset_info.shape_function,'quadratic')
+else
+    subset_struct.shape_fun = [ones(size(subset_struct.points_x(:))) subset_struct.points_x(:) subset_struct.points_y(:)];
+    subset_struct.order = 6;
+ 
+end
 
-subset_struct = 1;
 end
