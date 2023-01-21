f3408399fe56ad93e50acee125150c39aba297e3
Samuel Fayad <Samuel.S.Fayad@gmail.com>
2023-01-21 12:03:03 -0600

    updated the cost function to perform the SSD between the reference subset and interpolated deformed image

--- cost_fun.m
+++ cost_fun.m
@@ -1,3 +1,7 @@
-function chi = cost_fun(ref,def)
-chi = sum((ref-def).^2);
+function chi = cost_fun(alpha_opt,subset,point_of_interest,def_interp,ref)
+linearIndex = sub2ind(size(ref),subset.points_y(:)+  point_of_interest(2), subset.points_x(:)+ point_of_interest(1));
+
+chi = sum((def_interp(subset.shape_fun*alpha_opt(1:end/2) +subset.points_y(:) + point_of_interest(2),...
+    subset.shape_fun*alpha_opt(end/2+1:end) + subset.points_x(:) + point_of_interest(1))-...
+    ref(linearIndex)).^2);
 end
