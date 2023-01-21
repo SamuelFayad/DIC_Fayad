20839a029f0bf06d9dee5174e663b30363d0baaa
Samuel Fayad <Samuel.S.Fayad@gmail.com>
2023-01-21 15:01:18 -0600

    final formatting

--- main.m
+++ main.m
@@ -6,6 +6,7 @@ clc, clear, close all
 %% upload images
 [FILENAME_REF, PATHNAME_REF] = uigetfile('*.*');
 ref = double(imread(strcat(PATHNAME_REF,'/',FILENAME_REF)));
+[image_coords_j image_coords_i] = meshgrid(1:size(ref,2),1:size(ref,1));
 [FILENAME_DEF, PATHNAME_DEF] = uigetfile('*.*','Select deformed images','Multiselect','on');
 for image_ind = 1:length(FILENAME_DEF)
     def(:,:,image_ind) = double(imread(char(strcat(PATHNAME_DEF,'/',FILENAME_DEF(image_ind)))));
@@ -26,15 +27,13 @@ title('Draw Exclusion')
 %% define sample step size
 step_size = 12;
 
-%% define grid based on ROI and step size
+%% define analysis grid based on ROI and step size
 [grid_DIC_x grid_DIC_y] = meshgrid(1:step_size:size(ref,2),1:step_size:size(ref,1));
 points_in_ROI = inpolygon( grid_DIC_x , grid_DIC_y , j_px , i_px );
 points_in_ROI(inpolygon( grid_DIC_x , grid_DIC_y , j_px_exclude , i_px_exclude ))=0;
 grid_DIC_x = grid_DIC_x(points_in_ROI);
 grid_DIC_y = grid_DIC_y(points_in_ROI);
 
-%% define image interpolant
-[image_coords_j image_coords_i] = meshgrid(1:size(ref,2),1:size(ref,1));
 
 %% define subset shape, size, and shape function
 subset_info.shape = 'square'; % square or circle
@@ -42,13 +41,10 @@ subset_info.size = 25;
 subset_info.shape_function = 'affine'; % affine or quadratic
 subset = create_subset(subset_info);
 
-%% define initial guess
-% pass
-%% define cost function
-% see other file
-
 %% define optimization
 options = optimoptions('fminunc','Display','none');
+
+%% initialize values
 found_parameters = zeros(subset.order,length(grid_DIC_x),size(def,3));
 SSD = zeros(length(grid_DIC_x),size(def,3));
 DIC_u = zeros(length(grid_DIC_x),size(def,3));
@@ -58,8 +54,8 @@ Eyy = zeros(length(grid_DIC_x),size(def,3));
 Exy = zeros(length(grid_DIC_x),size(def,3));
 vsg_size = step_size*5;
 
-%% define post processing
-for image_ind = 1 %: size(def,3)
+%% Image processing loop
+for image_ind = 1 : size(def,3)
     f = waitbar(0,strcat('Analyzing image-',num2str(image_ind)));
     movegui(f);
     def_interp = griddedInterpolant(image_coords_i,image_coords_j,def(:,:,image_ind),'spline');
@@ -68,6 +64,7 @@ for image_ind = 1 %: size(def,3)
     for point_ind = 1 : length(grid_DIC_x)
         point_of_interest = [grid_DIC_x(point_ind) grid_DIC_y(point_ind)];
         [alpha_opt,fval,exitflag,output] = fminunc(@(alpha_opt) cost_fun(alpha_opt,subset,point_of_interest,def_interp,ref),x0(:,point_ind),options);
+        % assign values based on output of DIC
         found_parameters(:,point_ind,image_ind)=alpha_opt;
         SSD(point_ind,image_ind)=fval;
         waitbar(point_ind/length(grid_DIC_x),f)
@@ -75,6 +72,7 @@ for image_ind = 1 %: size(def,3)
     end
     DIC_u(:,image_ind) = found_parameters(4,:,image_ind);
     DIC_v(:,image_ind) = found_parameters(1,:,image_ind);
+    % compute strain
     [exx, eyy, exy] = compute_strain(grid_DIC_x,grid_DIC_y,DIC_u(:,image_ind),DIC_v(:,image_ind),vsg_size);
     Exx(:,image_ind) = exx;
     Eyy(:,image_ind) = eyy;
